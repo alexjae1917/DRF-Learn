fc51659e57c8cd63fee831154cb2dc64260a45ab
alexjae <alex777544@gmail.com>
2024-12-20 20:16:18 +0200

    2 commit

--- first/views.py
+++ first/views.py
@@ -1,1 +1,8 @@
-from rest_framework.decorators import
+from rest_framework import  status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def hello(request):
+    return Response(data={'Message':'hello'}, status=status.HTTP_200_OK)
